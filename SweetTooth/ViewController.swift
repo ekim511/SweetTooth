d4d003351ebef65e5ddbf28294bd16ce5bead1f8
ekim511 <ekim511@gmail.com>
2017-07-18 11:41:38 -0400

    Initial Commit

---
+++ SweetTooth/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SweetTooth
+//
+//  Created by Eliott Kim on 7/18/17.
+//  Copyright © 2017 Eliott Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
