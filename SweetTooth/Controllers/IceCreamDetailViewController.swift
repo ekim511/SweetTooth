be3480d46b1a664760a6216559706ab18e3e23ec
ekim511 <ekim511@gmail.com>
2017-07-19 11:52:20 -0400

    SweetTooth Added Segue
    Added feature: segue passing object to next VC but segue performing before async. function completed

--- SweetTooth/Controllers/IceCreamDetailViewController.swift
+++ SweetTooth/Controllers/IceCreamDetailViewController.swift
@@ -10,41 +10,27 @@ import UIKit
 
 class IceCreamDetailViewController: UIViewController {
 
+    var store : IceCreamStore?
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
+
     //MARK: - Properties
     
     @IBOutlet weak var storeNameLabel: UILabel!
-    
     @IBOutlet weak var repickRandomIceCreamButton: UIButton!
-
-    @IBAction func repickRandomIceCreamButtonTapped(_ sender: UIButton) {
-    }
     @IBOutlet weak var continueButton: UIButton!
     
+    @IBAction func repickRandomIceCreamButtonTapped(_ sender: UIButton) {
+        
+        
+    }
+    
     @IBAction func continueButtonTapped(_ sender: UIButton) {
+        
+        
     }
     
-    
-    
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
--- SweetTooth/Controllers/IceCreamSelectorViewController.swift
+++ SweetTooth/Controllers/IceCreamSelectorViewController.swift
@@ -11,41 +11,74 @@ import YelpAPI
 import BrightFutures
 
 class IceCreamSelectorViewController: UIViewController {
-
+    
+    let appId = "LRMAvc9fGrUR5riqTA5TPg"
+    let appSecret = "o3WyKYuDrv9ToWN71ruMHKUFWO6S6TNBrl0qcRpktkL3UpTIEe8qmRHrvvCVLeqR"
+    var businesses = [YLPBusiness]()
+    var store: IceCreamStore?
+    let group = DispatchGroup()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
     
     //MARK: - Properties
     
     @IBOutlet weak var iceCreamRandomButton: UIButton!
-    
-    @IBAction func iceCreamRandomButtonTapped(_ sender: UIButton) {
-    }
-    
     @IBOutlet weak var iceCreamButtonLabel: UILabel!
-    
     @IBOutlet weak var historyButton: UIButton!
     
     @IBAction func historyButtonTapped(_ sender: UIButton) {
+        
+        
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    
+    
+    
+    //Return array of businesses
+    func yelpQuery(){
+        let query = YLPQuery(location: "New York, NY")
+        query.term = "ice cream"
+        query.limit = 10
+        
+        YLPClient.authorize(withAppId: appId, secret: appSecret).flatMap { client in
+            client.search(withQuery: query)
+            }.onSuccess { search in
+                self.businesses = search.businesses
+                self.store = self.getStore()
+                self.group.leave()
+            }.onFailure { error in
+                print("Search errored: \(error)")
+                exit(EXIT_FAILURE)
+        }        
+    }
+    
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        if let identifier = segue.identifier {
+            if identifier == "displayDetailView" {
+                print("Random ice cream button tapped")
+                
+                group.enter()
+                self.yelpQuery()
+                group.notify(queue: DispatchQueue.global(qos: .background)) {
+                    let iceCreamDetailViewController = segue.destination as! IceCreamDetailViewController
+                    iceCreamDetailViewController.store = self.store
+                    print(iceCreamDetailViewController.store?.name)
+                }
+                
+                
+                
+                
+            }
+        }
     }
-    */
-
+    
+    
+    //Choose random business
+    func getStore() -> IceCreamStore {
+        let randomIndex = Int(arc4random_uniform(UInt32(self.businesses.count)))
+        let randomBusiness = self.businesses[randomIndex]
+        return IceCreamStore(object : randomBusiness)
+    }
 }
