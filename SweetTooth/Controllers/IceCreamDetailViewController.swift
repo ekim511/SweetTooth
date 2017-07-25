2f12ff30a9b8957649ac0f5e58bfc521d1fe47d2
ekim511 <ekim511@gmail.com>
2017-07-25 11:30:24 -0400

    SweetTooth with app icon
    Added feature: app icon

--- SweetTooth/Controllers/IceCreamDetailViewController.swift
+++ SweetTooth/Controllers/IceCreamDetailViewController.swift
@@ -8,9 +8,10 @@
 
 import UIKit
 import AlamofireImage
+import MessageUI
 
 
-class IceCreamDetailViewController: UIViewController {
+class IceCreamDetailViewController: UIViewController/*/, MFMessageComposeViewControllerDelegate */ {
     
     //MARK: - Properties
     
@@ -21,6 +22,7 @@ class IceCreamDetailViewController: UIViewController {
     @IBOutlet weak var locationLabel: UILabel!
 
     @IBOutlet weak var ratingLabel: UILabel!
+    
     var store : IceCreamStore?
     
     @IBOutlet weak var storePicImageView: UIImageView!
@@ -36,7 +38,7 @@ class IceCreamDetailViewController: UIViewController {
             locationLabel.text = location
         }
         if let rating = store?.rating {
-            ratingLabel.text = rating
+            ratingLabel.text = "Yelp Rating: \(rating)"
         }
         if let imageURLConst = store?.imageURL {
             loadPoster(urlString: imageURLConst)
@@ -55,10 +57,24 @@ class IceCreamDetailViewController: UIViewController {
     @IBAction func repickRandomIceCreamButtonTapped(_ sender: UIButton) {
         
     }
-    
+
 
     @IBAction func shareButonTapped(_ sender: UIButton) {
+       /* if (MFMessageComposeViewController.canSendText()) {
+            let controller = MFMessageComposeViewController()
+            controller.body = "Hi! I just found a great ice cream place! Here it is! \(store?.fullAddress)"
+            controller.recipients = []
+            controller.messageComposeDelegate = self
+            self.present(controller, animated: true, completion: nil)
+        }*/
     }
+    
+    /*func messageComposeViewController(_ controller: MFMessageComposeViewController!, didFinishWith result: MessageComposeResult) {
+        //... handle sms screen actions
+        self.dismiss(animated: true, completion: nil)
+    }*/
+    
+
 
       
     
